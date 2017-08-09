04d8a7600604e61675290833b5c2d759528296c7
Anastasia <Stacy.laushkina@gmail.com>
2017-08-09 20:11:58 +0700

    Add basic project structure

---
+++ TestYourSavvyApp/src/main/java/com/laushkina/anastasia/testyoursavvy/view/SelectTestActivity.java
@@ -0,0 +1,28 @@
+package com.laushkina.anastasia.testyoursavvy.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.laushkina.anastasia.testyoursavvy.R;
+
+public class SelectTestActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_test);
+    }
+
+    public void navigateToAnagram(View view) {
+        // TODO
+    }
+
+    public void navigateToVision(View view) {
+        // TODO
+    }
+
+    public void navigateToGPS(View view) {
+        // TODO
+    }
+}
